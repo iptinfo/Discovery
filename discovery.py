945ec91ea9be9126598c8008bb75fe536e9f10eb
IPT Info <36963007+iptinfo@users.noreply.github.com>
2018-03-23 09:53:49 -0500

    Update discovery.py

--- discovery.py
+++ discovery.py
@@ -19,7 +19,7 @@
 #  to perform a discovery of the CUCM/IMP cluster.
 #
 #  This scripts queries CUCM/IMP and does not write to either system.  No warranties, assurances or 
-#  support are being offered or implied.  Test thoroughly on non-productionprior to using in production. 
+#  support are being offered or implied.  Test thoroughly on non-production prior to using in production. 
 ####
 
 import ssl
